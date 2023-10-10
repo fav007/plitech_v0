6334815e7b2b511ed12b4bd4da429faf52d3664c
fav007 <besthorizon@hotmail.fr>
2023-10-10 08:08:05 +0300

    null znd blank

--- customers/models.py
+++ customers/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 class Customers(models.Model):
     name = models.CharField(max_length=200,unique=True)
-    company = models.CharField(max_length=200)
-    contact = models.CharField(max_length=10,unique=True)
-    location = models.CharField(max_length=100)
+    company = models.CharField(max_length=200,null=True,blank=True)
+    contact = models.CharField(max_length=10,unique=True,null=True,blank=True)
+    location = models.CharField(max_length=100,null=True,blank=True)
     
     def __str__(self) -> str:
         return f"{self.name} by {self.location}"
