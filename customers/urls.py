e6f66e3cf0cc0fe231b8aa122951262d64e3ea66
fav007 <besthorizon@hotmail.fr>
2023-10-24 09:04:19 +0300

    ok

--- customers/urls.py
+++ customers/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ListCustomers,CustomersDetailView,CustomerCreateView,CustomersUpdateView,CustomersDeleteView
+from .views import (HomePageView, AboutPageView, ListCustomers,CustomersDetailView,CustomerCreateView,CustomersUpdateView,CustomersDeleteView,
+                    PricingPageView)
 
 urlpatterns = [
 path('', HomePageView.as_view(), name='home'),
 path('about/', AboutPageView.as_view(),name='about'),
+path('pricing/', PricingPageView.as_view(),name='price-list'),
 path('create/', CustomerCreateView.as_view(), name='customer-create'),
 path('list/' ,ListCustomers.as_view(),name='customer-list'),
 path('list/<int:pk>/', CustomersDetailView.as_view(), name='customer-detail'),
